13455b338829c2c37c99715ffb558443282bc5bc
Tina Hosseinpour <Tinahosseinpour.79@gmail.com>
2022-03-30 05:31:52 -0700

    form component added!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/form.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Form,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  font-family: "Jost", sans-serif;
+  background: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
 }
 </style>
+
---
+++ src/components/form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="main"></div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+#main {
+  background: linear-gradient(-180deg, #85a2be 0%, #7ca7db 98%),
+    radial-gradient(
+      at top left,
+      rgba(255, 255, 255, 0.3) 0%,
+      rgba(0, 0, 0, 0.3) 100%
+    );
+  background-blend-mode: screen;
+  width: 350px;
+  height: 500px;
+  border-radius: 15px;
+  box-shadow: 5px 20px 50px #000;
+}
+</style>
