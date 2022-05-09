c4f64f55c711ec19ec020427587f177cfb98298c
Tina Hosseinpour <Tinahosseinpour.79@gmail.com>
2022-05-09 00:07:52 -0700

    tasks

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,43 @@
 <script>
 export default {
   methods: {},
-  
+  created() {
+    // let numbers = [2, 5, 6, 4, 3];
+    // let item = 9;
+
+    // function myFilter(array, num) {
+    //   for (let i = 0; i < array.length; i++) {
+    //     // console.log(array[i])
+    //     for (let n = 0; n < array.length; n++) {
+    //       // console.log(array[i])
+    //       // console.log(array[n])
+    //       if (array[n] + array[i] === num) {
+    //         console.log(i);
+    //         // console.log(n)
+    //         // console.log('true')
+    //       } else {
+    //         console.log("false");
+    //       }
+    //     }
+    //   }
+    // }
+
+    // myFilter(numbers, item);
+
+    function changeToReverse(number) {
+      number = number + "";
+      return number.split("").reverse().join("");
+    }
+
+    let item = 181;
+    let reversedNum = Number(changeToReverse(item));
+    console.log(reversedNum);
+    if (item == reversedNum) {
+      console.log("true");
+    } else {
+      console.log("fasle");
+    }
+  },
 };
 </script>
 
